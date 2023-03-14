510eff0bdc6dad0b79b2eaf2d9687202d15eddc0
rjud0002 <rjud0002@student.monash.edu>
2023-03-14 17:01:18 +1100

    initial commit

---
+++ playground-2/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  playground-2
+//
+//  Created by Rizki Judojono on 14/3/2023.
+//
+
+import SwiftUI
+import SocketIO
+
+final class Service: ObservableObject {
+    private var manager = SocketManager(socketURL: URL(string: "ws://172.104.46.87:8080")!, config: [.log(true), .compress])
+        
+    @Published var messages = [String]()
+    
+    init() {
+        let socket = manager.defaultSocket
+        socket.on(clientEvent: .connect) { (data, ack) in
+            print("Connected!")
+            socket.emit("my_event", "Hey this is iPhone! Nanana!")
+        }
+        
+        socket.on("my_response") { [weak self] (data, ack) in
+            if let data = data[0] as? [String: String],
+               let rawMessage = data["response"] {
+                DispatchQueue.main.async {
+                    self?.messages.append(rawMessage)
+                }
+            }
+        }
+        
+        func sendSomething() {
+            socket.emit("my_event", "Hey this is iPhone! Nanana!")
+        }
+        
+        socket.connect()
+    }
+}
+
+
+struct ContentView: View {
+    @ObservedObject var service = Service()
+    
+    var body: some View {
+        VStack {
+            
+            Text("Received")
+                .font(.largeTitle)
+            ForEach(service.messages, id: \.self) { msg in
+                Text(msg).padding()
+            }
+            Spacer()
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text("Hello, world!")
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ playground-2/playground_2App.swift
@@ -0,0 +1,17 @@
+//
+//  playground_2App.swift
+//  playground-2
+//
+//  Created by Rizki Judojono on 14/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct playground_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
